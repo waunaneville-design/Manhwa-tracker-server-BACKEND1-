b3d0b6d3dfda78dc7772b99919f417023c356e8e
Lascelles <waunaneville@gmail.com>
2026-07-31 15:53:59 +0300

    add an express component

--- server.js
+++ server.js
@@ -36,6 +36,8 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('✅ MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
+const userRoutes = require('./routes/userRoutes');
+app.use('/api/users', userRoutes);
 
 // routes
 app.use('/api', authRoutes);
